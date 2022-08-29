c7c1bfba84acc7653d42d115c94a106927bf5868
Yasen Kurtev <83857426+YasenKurtev@users.noreply.github.com>
2022-08-29 13:14:04 +0300

    Added completed tasks and their descriptions from the current lesson.

---
+++ JS Applications Exam - 2 April 2022/src/handlers/loginHandler.js
@@ -0,0 +1,59 @@
+import { html } from "../../node_modules/lit-html/lit-html.js";
+import { login } from "../services/authService.js";
+import { setUser } from "../services/userService.js";
+
+let loginTemplate = (onSubmit) => html`
+<section id="loginPage">
+    <form class="loginForm" @submit=${onSubmit}>
+        <img src="./images/logo.png" alt="logo" />
+        <h2>Login</h2>
+
+        <div>
+            <label for="email">Email:</label>
+            <input id="email" name="email" type="text" placeholder="steven@abv.bg" value="">
+        </div>
+
+        <div>
+            <label for="password">Password:</label>
+            <input id="password" name="password" type="password" placeholder="********" value="">
+        </div>
+
+        <button class="btn" type="submit">Login</button>
+
+        <p class="field">
+            <span>If you don't have profile click <a href="#">here</a></span>
+        </p>
+    </form>
+</section>
+`
+
+export let loginHandler = (ctx) => {
+    ctx.render(loginTemplate(onSubmit));
+
+    function onSubmit(ev) {
+        ev.preventDefault();
+        let formData = new FormData(ev.target);
+        let email = formData.get('email');
+        let password = formData.get('password');
+
+        if (email !== '' && password !== '') {
+            login(email, password)
+                .then(res => {
+                    if (!res.ok) {
+                        ev.target.reset();
+                        throw new Error('Ivalid email or password!!!');
+                    } else {
+                        return res.json();
+                    }
+                })
+                .then(user => {
+                    setUser(user);
+                    ev.target.reset();
+                    ctx.page.redirect('/')
+                })
+                .catch(err => alert(err));
+        } else {
+            alert('There are empty fields!');
+        }
+    }
+}
